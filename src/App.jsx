2415f91d7545c1291a816e90e2695478c22ae769
KaloyanBouresh <kalata2316@gmail.com>
2022-10-18 17:05:41 +0300

    firebase setup + pages + routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-      <hr />
-      <h1 style={{ color: "red" }}>
-        Hello
-      </h1>
+      <Router>
+        <Routes>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/offers' element={<Offers />}></Route>
+          <Route path='/profile' element={<SignIn />}></Route>
+          <Route path='/frogot-password' element={<ForgotPassword />}></Route>
+          <Route path='/' element={<Explore />}></Route>
+        </Routes>
+        < Navbar />
+      </Router>
     </>
   );
 }
