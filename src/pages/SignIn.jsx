fb4ab567180715e6c9448a7bb24ff95509120212
KaloyanBouresh <kalata2316@gmail.com>
2022-10-19 17:15:22 +0300

    google auth implemented

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import OAuth from '../components/OAuth';
 
 function SignIn() {
     const [showPassword, setShowPassword] = useState(false);
@@ -89,7 +90,8 @@ function SignIn() {
                             </button>
                         </div>
                     </form>
-                    {/* Google OAuth */}
+
+                    <OAuth />
 
                     <Link to='/sign-up' className='registerLink'>
                         Sign Up Instead?
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,6 +7,7 @@ import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { db } from '../firebase.config'
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import OAuth from '../components/OAuth';
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -61,7 +62,7 @@ function SignUp() {
       <div className="pageContainer">
         <header>
           <p className="pageHeader">
-            Welcome Back!
+            Welcome!
           </p>
         </header>
 
@@ -110,7 +111,8 @@ function SignUp() {
               </button>
             </div>
           </form>
-          {/* Google OAuth */}
+          <OAuth />
+
           <Link to='/sign-in' className='registerLink'>
             Sign In Instead?
           </Link>
